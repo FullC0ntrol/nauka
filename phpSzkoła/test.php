dfab2e5bae3b2c8794c29dbb4c49f8bf79d6de88
FullC0ntrol <104495512+FullC0ntrol@users.noreply.github.com>
2022-11-15 12:34:35 +0100

    a

--- phpSzkoła/test.php
+++ phpSzkoła/test.php
@@ -9,4 +9,8 @@ echo gettype($bool) . "<br>";
 echo gettype($float) . "<br>";
 echo gettype($int) . "<br>";
 echo gettype($text);
+$slowo1 = "World!";
+$slowo2 = "Dolly";
+$slowo3 = "Hello World! LALLA LALLALA LALLA";
+echo str_replace($slowo1, $slowo2, $slowo3);
 ?>
